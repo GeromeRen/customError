8cdeb21eabf02beff02559f32f60eca257aa2c99
GeromeRen <rencyu@gmail.com>
2017-03-28 01:08:22 +0800

    Update author name
    Signed-off-by: 'Chunyu.ren' <'Chunyu.ren@accenture.com'>

--- src/abstractError.js
+++ src/abstractError.js
@@ -3,11 +3,10 @@ var util = require('util');
 
 var _AbstractError = function (targetSetting, constr){
     targetSetting = targetSetting || {};
-    this.name = 'CustomError';
     this.type = targetSetting.type || 'Application';
     this.message = targetSetting.message || 'An error occurred'
     this.detail = targetSetting.detail || '';
-    this.extendedInfo = targetSetting.extendedInfo || '';
+    this.extra = targetSetting.extra || '';
     this.errCode = targetSetting.errCode || '';
     this.rootCause = targetSetting.rootCause || null;
     this.isAppErr = true;
